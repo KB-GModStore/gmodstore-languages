20dff86fdea7b53abcff1ba2562df70b2126efeb
AlexandreVea <117751125+AlexandreVea@users.noreply.github.com>
2024-05-19 22:01:36 +0200

    Italian Language

---
+++ zeros_crackermachine/zcm_languages/sh_zcm_language_it.lua
@@ -0,0 +1,18 @@
+zcm = zcm or {}
+zcm.language = zcm.language or {}
+zcm.language.General = zcm.language.General or {}
+
+if (zcm.config.SelectedLanguage == "it") then
+    zcm.language.General["Start"] = "Avvia"
+    zcm.language.General["Stop"] = "Ferma"
+    zcm.language.General["Level"] = "Livello"
+    zcm.language.General["MaxLevel"] = "Livello Massimo"
+    zcm.language.General["Sell"] = "Vendi"
+    zcm.language.General["ReachedMaxLevel"] = "La macchina ha raggiunto il livello massimo!"
+    zcm.language.General["NotEnoughMoney"] = "Non hai abbastanza soldi!"
+    zcm.language.General["MachineUpgraded"] = "Macchina migliorata!"
+    zcm.language.General["NPCTitle"] = "Venditore di fuochi d'artificio"
+    zcm.language.General["Firework"] = "Fuoco d'artificio"
+    zcm.language.General["Collect"] = "Raccogli"
+    zcm.language.General["Wrong Job"] = "Lavoro sbagliato!"
+end
