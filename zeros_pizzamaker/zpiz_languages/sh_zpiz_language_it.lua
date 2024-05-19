d9d9b13cb1bf3f9fe5c07dc711dd496ed5f1f6bb
AlexandreVea <117751125+AlexandreVea@users.noreply.github.com>
2024-05-19 18:19:34 +0200

    Italian Language

---
+++ zeros_pizzamaker/zpiz_languages/sh_zpiz_language_it.lua
@@ -0,0 +1,26 @@
+zpiz = zpiz or {}
+zpiz.language = zpiz.language or {}
+
+if (zpiz.config.SelectedLanguage == "it") then
+    zpiz.language.VGUI_Recipe = "Ricette"
+    zpiz.language.VGUI_RecipeBakeTime = "Tempo di cottura:"
+    zpiz.language.VGUI_RecipeProfit = "Profitto:"
+    zpiz.language.VGUI_ShopTitle = "Negozio di ingredienti"
+    zpiz.language.Customer_ServPizza_good = "Grazie!"
+    zpiz.language.Customer_ServPizza_bad = "È bruciata ma la prendo comunque."
+    zpiz.language.Customer_Pays = "Pizza venduta!"
+    zpiz.language.FridgeShop_NoMoney = "Non hai abbastanza denaro!"
+    zpiz.language.FridgeShop_BoughtItem = "Hai comprato un/una $ItemName per $ItemPrice"
+    zpiz.language.FridgeShop_LimitReached = "Hai raggiunto il limite di ingredienti!"
+    zpiz.language.OpenSign_open = "Aperto per affari"
+    zpiz.language.OpenSign_closed = "Chiuso"
+    zpiz.language.OpenSign_Revenue = "Ricavi: "
+    zpiz.language.OpenSign_RevenueMessage = "I tuoi ricavi fino ad ora sono "
+    zpiz.language.Oven_PizzaBaking = "Cottura in corso..."
+    zpiz.language.Oven_PizzaReady = "Premi ´Usa´ per prelevare la pizza"
+    zpiz.language.Oven_PizzaBurned = "Bruciata!"
+    zpiz.language.PizzaConsum_Good = {"Deliziosa!"}
+    zpiz.language.PizzaConsum_Bad = {"Potrebbe avere un cattivo sapore"}
+    zpiz.language.Pizza_Ready = "Una pizza è pronta!"
+    zpiz.language.Pizza_Burned = "Una pizza è appena bruciata!"
+end
