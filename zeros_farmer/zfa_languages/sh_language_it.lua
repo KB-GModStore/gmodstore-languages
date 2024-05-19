0fb397f5e43db8097abd497920e3c0fc5c4ea5c1
AlexandreVea <117751125+AlexandreVea@users.noreply.github.com>
2024-05-19 21:53:37 +0200

    Italian Language

---
+++ zeros_farmer/zfa_languages/sh_language_it.lua
@@ -0,0 +1,83 @@
+zfa = zfa or {}
+zfa.language = zfa.language or {}
+
+if (zfa.config.SelectedLanguage == "fr") then
+    zfa.language["EquipmentCrate"] = "Cassa attrezzature"
+    zfa.language["NONE"] = "NESSUNO"
+    zfa.language["Purchase"] = "Acquistare"
+    zfa.language["Seeds"] = "Semi"
+    zfa.language["Show Zones"] = "Mostra zone"
+    zfa.language["Unload"] = "Scaricare"
+    zfa.language["Sell"] = "Vendere"
+    zfa.language["FULL"] = "PIENO"
+    zfa.language["EMPTY"] = "VUOTO"
+    zfa.language["HayBlocks"] = "Blocchi di fieno"
+    zfa.language["HarvestCrateLimit"] = "Hai raggiunto il limite di casse di raccolta!"
+    zfa.language["NoCropBuyerFound"] = "Nessun acquirente trovato!"
+    zfa.language["WrongJob"] = "Non sei un contadino!"
+    zfa.language["NoCrop"] = "Torna quando hai delle colture da vendere!"
+    zfa.language["EntityLimit"] = "Limite di entità raggiunto per $EntityName!"
+    zfa.language["NotEnoughMoney"] = "Non abbastanza soldi!"
+    zfa.language["Turn"] = "Girare"
+    zfa.language["OFF"] = "SPEGNERE"
+    zfa.language["ON"] = "ACCENDERE"
+    zfa.language["Deconnect"] = "Rimuovere"
+    zfa.language["Connect"] = "Collegare"
+    zfa.language["NoAttachment"] = "Nessun attacco"
+    zfa.language["NoAttachmentFound"] = "Nessun attacco trovato!"
+    zfa.language["Zone"] = "Zona"
+    zfa.language["AreaNotFree"] = "La zona non è libera!"
+    zfa.language["Spawning"] = "Generazione"
+    zfa.language["WrongRank"] = "Rango sbagliato!"
+    zfa.language["WrongJob"] = "Mestiere sbagliato!"
+
+    // Questo significa solo che la zona specificata pianterà automaticamente piante nel tempo. Se hai un nome migliore, fai pure.
+    zfa.language["AutoPlant"] = "Piantagione automatica"
+
+    // Questo significa che la zona non ha restrizioni di piantagione e il giocatore può piantare quello che vuole all'interno della zona
+    zfa.language["Any"] = "Qualsiasi"
+
+    zfa.language["Harvesterfull"] = "La mietitrebbia è piena!"
+
+    // Aggiornamento 1.1.0
+    zfa.language["FieldMode"] = "Campo"
+    zfa.language["ChopMode"] = "Tagliare"
+    zfa.language["ShowState"] = "Mostra stato"
+
+
+    // Aggiornamento 1.1.3
+    zfa.language["YouDontOwnThis"] = "Non possiedi questo!"
+    zfa.language["SellValue:"] = "Valore di vendita:"
+    zfa.language["Growtime:"] = "Tempo di crescita:"
+
+    zfa.language["desc_tractor_tib"] = "Il veicolo principale per la raccolta del tiberio."
+    zfa.language["desc_harvester_tib"] = "Una volta attaccato al trattore, questo modulo è pronto per la raccolta del tiberio."
+    zfa.language["desc_trailer_tib"] = "Usato per vendere i cristalli raccolti."
+    zfa.language["desc_tractor"] = "Il veicolo principale per l'agricoltura."
+    zfa.language["desc_culti"] = "Questa macchinaria è usata per preparare il terreno alla coltivazione."
+    zfa.language["desc_planter"] = "Questa macchinaria pianta semi su campi coltivati."
+    zfa.language["desc_harvester"] = "Questa macchina raccoglie le piante completamente sviluppate."
+    zfa.language["desc_haypress"] = "Questa macchina raccoglie mucchi di fieno e li comprime in blocchi."
+    zfa.language["desc_haystacker"] = "Questa macchina raccoglie i blocchi di fieno."
+    zfa.language["desc_trailer"] = "Questa macchina è usata per il trasporto e la vendita dei raccolti."
+    zfa.language["desc_sprayer"] = "Questa macchina è usata per trattare i campi con una varietà di liquidi."
+
+    zfa.language["desc_liquid_water"] = "Le piante hanno bisogno di acqua."
+    zfa.language["desc_liquid_pesticide"] = "Elimina gli insetti dal campo e protegge le piante da essi."
+    zfa.language["desc_liquid_fertilizer01"] = "Aumenta leggermente la velocità di crescita."
+    zfa.language["desc_liquid_fertilizer02"] = "Aumenta molto la velocità di crescita."
+
+    zfa.language["title_tractor"] = "Trattore"
+    zfa.language["title_harvester"] = "Mietitrebbia"
+    zfa.language["title_trailer"] = "Rimorchio"
+    zfa.language["title_culti"] = "Coltivatrice"
+    zfa.language["title_planter"] = "Semina"
+    zfa.language["title_haypress"] = "Pressa per fieno"
+    zfa.language["title_haystacker"] = "Impilatrice di fieno"
+    zfa.language["title_sprayer"] = "Spruzzatrice"
+
+    zfa.language["title_liquid_water"] = "Acqua"
+    zfa.language["title_liquid_pesticide"] = "Pesticidi"
+    zfa.language["title_liquid_fertilizer01"] = "Fertilizzante - Normale"
+    zfa.language["title_liquid_fertilizer02"] = "Fertilizzante - Estremo"
+end
