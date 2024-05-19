7b20acb8355dbfa55775280566229986d2d45642
AlexandreVea <117751125+AlexandreVea@users.noreply.github.com>
2024-05-19 20:04:13 +0200

    Italian Language

---
+++ zeros_yeastbeast/zyb_languages/sh_zyb_language_it.lua
@@ -0,0 +1,52 @@
+zyb = zyb or {}
+zyb.language = zyb.language or {}
+zyb.language.General = zyb.language.General or {}
+zyb.language.NPC = zyb.language.NPC or {}
+
+zyb.language.VGUI = zyb.language.VGUI or {}
+
+if (zyb.config.SelectedLanguage == "it") then
+
+    zyb.language.General["Distillery"] = "Distilleria"
+    zyb.language.General["MissingCooler"] = "Raffreddatore mancante!"
+    zyb.language.General["MissingCondenser"] = "Condensatore mancante!"
+    zyb.language.General["YeastStew"] = "Impasto di lievito"
+    zyb.language.General["Fuel"] = "Carburante"
+    zyb.language.General["Depressurize"] = "Depressurizzare"
+    zyb.language.General["Condenser"] = "Condensatore"
+    zyb.language.General["Cooler"] = "Raffreddatore"
+    zyb.language.General["ReleaseWater"] = "Rilascia acqua"
+    zyb.language.General["Fermentation Barrel"] = "Barile di fermentazione"
+    zyb.language.General["Yeast"] = "Lievito"
+    zyb.language.General["Sugar"] = "Zucchero"
+    zyb.language.General["Water"] = "Acqua"
+    zyb.language.General["Shakeit"] = "Agitalo!"
+    zyb.language.General["Fermenting"] = "Fermentazione!"
+    zyb.language.General["Cleanit"] = "Puliscilo!"
+    zyb.language.General["WrongUserGroup"] = "Gruppo di utenti errato!"
+    zyb.language.General["NoMoney"] = "Non hai abbastanza soldi!"
+    zyb.language.General["BuyInfo"] = "Hai acquistato l'aggiornamento $ItemName per $Currency$ItemPrice!"
+    zyb.language.General["SellInfo"] = "Hai venduto l'aggiornamento $ItemName per $Currency$ItemPrice!"
+    zyb.language.General["ConstrtuctionCancelled"] = "Costruzione annullata!"
+    zyb.language.General["MoonshineJars"] = "Vasetti di liquore illegale!"
+    zyb.language.General["Collect"] = "Raccogli"
+    zyb.language.General["Extract"] = "Estrai"
+    zyb.language.General["Inventoryempty"] = "L'inventario del liquore è vuoto!"
+
+    zyb.language.NPC["MoonshineBuyer"] = "Acquirente di liquore illegale"
+    zyb.language.NPC["Jar"] = "Vasetto"
+    zyb.language.NPC["NoMoonshine"] = "Torna quando hai qualcosa per me!"
+    zyb.language.NPC["WrongJob"] = "Mestiere sbagliato!"
+    zyb.language.NPC["SellInfo"] = "Hai venduto $Amount vasetti di liquore illegale per $Currency$MoneyAmount!"
+
+    zyb.language.VGUI["Speed"] = "Velocità"
+    zyb.language.VGUI["Production"] = "Produzione"
+    zyb.language.VGUI["Ranks"] = "Gradi"
+    zyb.language.VGUI["UpgradeLimitReached"] = "Limite di aggiornamento raggiunto"
+    zyb.language.VGUI["UpgradeTypeLimitReached"] = "Non puoi avere più aggiornamenti dello stesso tipo!"
+    zyb.language.VGUI["UpgradeMenu"] = "Menu di aggiornamento"
+    zyb.language.VGUI["Shop"] = "Negozio"
+    zyb.language.VGUI["Installed"] = "Installato"
+    zyb.language.VGUI["Buy"] = "Compra"
+    zyb.language.VGUI["Sell"] = "Vendi"
+end
