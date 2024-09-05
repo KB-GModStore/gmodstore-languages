ddcbf97f2e16cec60de16c93bea00c4f1869e85a
Marius <122283253+pyrmamarius@users.noreply.github.com>
2024-09-05 19:58:45 +0200

    Update sh_zrms_language_de.lua

--- zeros_retrominer/zrms_languages/sh_zrms_language_de.lua
+++ zeros_retrominer/zrms_languages/sh_zrms_language_de.lua
@@ -14,40 +14,40 @@ if(zrmine.config.selectedLanguage == "de")then
 
     zrmine.language.Basket_Full = "Voll!"
 
-    zrmine.language.Crate_Full = "Kiste ist Voll!"
+    zrmine.language.Crate_Full = "Die Kiste ist voll!"
 
-    zrmine.language.NPC_Title = "Metal Käufer"
-    zrmine.language.NPC_SellProfit = "Verkaufs Profit: "
-    zrmine.language.NPC_CashPerBar = "Geld Pro Baren"
-    zrmine.language.NPC_NoProduct = "Komm wieder wen du was für mich hast!"
+    zrmine.language.NPC_Title = "Kohle Klaus"
+    zrmine.language.NPC_SellProfit = "Profit: "
+    zrmine.language.NPC_CashPerBar = "Geld pro Barren"
+    zrmine.language.NPC_NoProduct = "Komm wieder wenn du was für mich hast!"
     zrmine.language.NPC_GoAway = "Geh weg!"
 
     zrmine.language.Module_DontOwn = "Das gehört dir nicht!"
     zrmine.language.Module_NoSpace = "Das passt hier nicht hin!"
 
-    zrmine.language.Crusher_Crushing = "Zermahle Erz.."
-    zrmine.language.Crusher_Waiting = "Warte auf Erz"
-    zrmine.language.Crusher_Full = "Erz Mühle ist Leer!"
+    zrmine.language.Crusher_Crushing = "Zerkleinere Erz..."
+    zrmine.language.Crusher_Waiting = "Warte auf Erz..."
+    zrmine.language.Crusher_Full = "Der Zerkleinerer ist voll!"
 
-    zrmine.language.Melter_InsertOre = "Bereit für Erz"
-    zrmine.language.Melter_NeedCoal = "Braucht mehr Kohle!"
+    zrmine.language.Melter_InsertOre = "Warte auf Erz..."
+    zrmine.language.Melter_NeedCoal = "Benötigt mehr Kohle!"
     zrmine.language.Melter_Melting = "Schmelze.."
-    zrmine.language.Melter_Full = "Hochofen ist Voll!"
+    zrmine.language.Melter_Full = "Der Hochofen ist voll!"
 
-    zrmine.language.Mine_Mining = "Schürfe Erz.."
-    zrmine.language.Mine_Ready = "Lore ist bereit!"
-    zrmine.language.Mine_Wait = "Warte.."
-    zrmine.language.Mine_SearchOre = "Drücke ´USE´ um nach Erz zu suchen!"
-    zrmine.language.Mine_NoOre = "Kein Erz hier gefunden!"
-    zrmine.language.Mine_EntLimit = "Du hast das Erz-brocken Limit für diese Mine erreicht!"
-    zrmine.language.Mine_Empty = "Verbundenes Erz Vorkommen ist Leer!"
+    zrmine.language.Mine_Mining = "Suche Erz..."
+    zrmine.language.Mine_Ready = "Bereit loszufahren!"
+    zrmine.language.Mine_Wait = "Warte..."
+    zrmine.language.Mine_SearchOre = "Drücke E um Erz zu suchen!"
+    zrmine.language.Mine_NoOre = "Kein Erz gefunden!"
+    zrmine.language.Mine_EntLimit = "Du hast das Limit für diese Mine erreicht!"
+    zrmine.language.Mine_Empty = "Das Erz ist leer!"
 
-    zrmine.language.Refiner_Refining = "Verfeinere Erz.."
-    zrmine.language.Refiner_Waiting = "Warte auf Erz"
-    zrmine.language.Refiner_Moving = "Fördere Erz.."
+    zrmine.language.Refiner_Refining = "Verfeinere Erz..."
+    zrmine.language.Refiner_Waiting = "Warte auf Erz.."
+    zrmine.language.Refiner_Moving = "Fördere Erz..."
 
-    zrmine.language.StorageCrate_Collect = "+ LagerKiste"
-    zrmine.language.StorageCrate_NotFull = "Die Kiste noch nicht voll!"
+    zrmine.language.StorageCrate_Collect = "+ Lagerkiste"
+    zrmine.language.StorageCrate_NotFull = "Die Kiste ist noch nicht voll!"
     zrmine.language.StorageCrate_NeedMetalBar = "Die Kiste ist leer!"
 
     zrmine.language.WrongJob = "Das kannst du nicht benutzen!"
@@ -55,17 +55,17 @@ if(zrmine.config.selectedLanguage == "de")then
     zrmine.language.Pickaxe_InvFull = "Dein Erz Inventar ist voll!"
 
     zrmine.language.LvlSys_LvlUp = "Level Aufstieg!"
-    zrmine.language.LvlSys_LvlMax = "Du hast das Maximal Level erreicht!"
-    zrmine.language.LvlSys_LvlReset = "Dein Level wurde von einem Admin Resetet!"
+    zrmine.language.LvlSys_LvlMax = "Du hast das maximale Level erreicht!"
+    zrmine.language.LvlSys_LvlReset = "Dein Level wurde von einem Admin zurückgesetzt!"
 
     zrmine.language.Cmd_NoPermission = "Du hast nicht die nötige Berechtigung dafür, bitte kontaktiere einen Admin."
 
     zrmine.language.Buy = "Kaufen" -- The action key for building
     zrmine.language.Sell = "Verkaufen" -- The action key for removing
     zrmine.language.SwitchItem = "Wechseln" -- The action key for switching the currently selected item
-    zrmine.language.CantBuild_ObjectHasChild = "Sie können keine Module mit ausgehenden Verbindungen dekonstruieren!" -- When the players tries to remove a module from the pipeline that has a child object
+    zrmine.language.CantBuild_ObjectHasChild = "Du kannst keine Module mit ausgehenden Verbindungen dekonstruieren!" -- When the players tries to remove a module from the pipeline that has a child object
     zrmine.language.NotEnoughMoney = "Das kannst du dir nicht leisten!"
-    zrmine.language.BuyInfo = "Du hast $ItemName für $Price gekauft!"
+    zrmine.language.BuyInfo = "Du hast das Item '$ItemName' für $Price gekauft!"
 
     zrmine.language.EntityLimitReached = "Entity Limit erreicht!"
 
