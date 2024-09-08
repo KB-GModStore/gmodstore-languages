62c605a57658b687dd1d89c71b06249302b7697e
Marius <122283253+pyrmamarius@users.noreply.github.com>
2024-09-08 19:25:51 +0200

    Update sh_ztm_language_de.lua

--- zeros_trashman/ztm_languages/sh_ztm_language_de.lua
+++ zeros_trashman/ztm_languages/sh_ztm_language_de.lua
@@ -19,7 +19,7 @@ if (ztm.config.SelectedLanguage == "de") then
     ztm.language.General["Trash"] = "Müll"
     ztm.language.General["Max"] = "Max" // Maximal Level reached
 
-    ztm.language.General["Blast"] = "Blast" // Primary trashgun action
+    ztm.language.General["Blast"] = "Pusten" // Primary trashgun action
     ztm.language.General["Suck"] = "Saugen" // Secondary trashgun action
 
     ztm.language.General["WrongJob"] = "Falscher Job!"
